605f3d65c1342be7fdd2888992526548fc968bf2
shayandutta <shayandutta23@gmail.com>
2025-07-06 21:08:12 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import Footer from "./components/footer/Footer"
 import Header from "./components/Header"
 
 function App() {
     return (
-        <div className="App">
+        <>
             <Header />
-        </div>
+            <Footer/>
+        </>
     )
 }
 
